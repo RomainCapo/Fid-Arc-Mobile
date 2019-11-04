5306a92add53e0d0f0814420f57336a0db0d8a9f
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2019-11-04 22:11:29 +0100

    Create FidarcAPIService

---
+++ app/src/main/java/ch/hearc/fidarc/ui/login/data/model/Test.kt
@@ -0,0 +1,7 @@
+package ch.hearc.fidarc.ui.login.data.model
+
+data class Test(
+    val id: Int = 0,
+    val title: String = "",
+    val completed: Boolean = false
+)
---
+++ app/src/main/java/ch/hearc/fidarc/ui/network/FidarcAPIService.kt
@@ -0,0 +1,32 @@
+package ch.hearc.fidarc.ui.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Path
+
+import ch.hearc.fidarc.ui.login.data.model.Test
+
+private const val BASE_URL = "https://jsonplaceholder.typicode.com/" //TODO : Remove this with the good api
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface FidarcAPIService {
+
+    @GET("/todos/{id}")
+    suspend fun getTest(@Path(value = "id") todoId: Int): Test
+}
+
+object FidarcAPI {
+    val retrofitService : FidarcAPIService by lazy { retrofit.create(FidarcAPIService::class.java) }
+}
