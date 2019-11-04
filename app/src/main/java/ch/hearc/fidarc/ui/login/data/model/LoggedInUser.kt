6105ec39b2283a777726a9a0fcbcd75663c533e1
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2019-11-04 21:21:30 +0100

    Create login template

---
+++ app/src/main/java/ch/hearc/fidarc/ui/login/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package ch.hearc.fidarc.ui.login.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
