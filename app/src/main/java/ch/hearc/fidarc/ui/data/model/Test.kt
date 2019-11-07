a6bdd743cac4421ecdb7b7cdd0949755f24c7429
Alexandre Bianchi <alexandre.bianchi@he-arc.ch>
2019-11-07 09:06:13 +0100

    Modify User model to corresponding API field

--- app/src/main/java/ch/hearc/fidarc/ui/data/model/Test.kt
+++
@@ -1,7 +0,0 @@
-package ch.hearc.fidarc.ui.data.model
-
-data class Test(
-    val id: Int = 0,
-    val title: String = "",
-    val completed: Boolean = false
-)
--- app/src/main/java/ch/hearc/fidarc/ui/data/model/User.kt
+++ app/src/main/java/ch/hearc/fidarc/ui/data/model/User.kt
@@ -3,9 +3,10 @@ package ch.hearc.fidarc.ui.data.model
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String,
-    val role: String,
-    val token: String
+data class User(
+    val id: Int,
+    val name: String,
+    val lastname: String,
+    val email: String,
+    val role_names: List<String>
 )
